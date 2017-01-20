47258c9a17271f5354ea946371794122d0ddd9d0
agrim mittal <agrimmittal97@gmail.com>
2017-01-20 19:21:00 +0530

    customize brain

--- bot_cli.py
+++ bot_cli.py
@@ -3,12 +3,9 @@ from brain import *
 def bot_cli():
 	while(1):
 		command = raw_input("Bot >>  ")
-		if command == 'yo':
-			print "YO !!"
-		elif any(command in s for s in greetings):
-			print "Whats up?"
-		elif command =="exit":
+		valid_response = brain(command)
+		if valid_response:
+			print valid_response
+		else:
 			print "Bye Bye!!"
 			exit()
-		else:
-			print "Unknown"
--- brain.py
+++ brain.py
@@ -1,2 +1,32 @@
-def brain():
-	greetings = ["hello","hi","yo"]
+import random
+import os
+import platform
+
+greetings = ["hello","hi","yo"]
+greetings_response = ["whats up?","how u doing?","how's day going?","yoo!!"]
+exit_command = ["exit","quit","close","leave"]
+sys_command = ["sysinfo"]
+
+def sys_response(command):
+	sys_command_list[command]()
+
+def sysinfo():
+	print os.getlogin()
+	print platform.machine()
+	print platform.version()
+	print platform.system()
+	print platform.processor()
+
+sys_command_list = {
+	"sysinfo": sysinfo
+}
+
+def brain(command):
+	if any(command in s for s in greetings):
+		return random.choice(greetings_response)
+	elif any(command in s for s in sys_command):
+		return sys_response(command)
+	elif any(command in s for s in exit_command):
+		return 0
+	else:
+		return "Unkown Command"
